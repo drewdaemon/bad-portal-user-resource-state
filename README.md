2851649e45eb3f1ff180825491428cbd0b4e3556
Andrew Tate <drewctate@gmail.com>
2021-04-28 18:59:10 -0500

    user-app-resource-demo

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# Portal API Access Level Latency Demonstration
+# Corrupt User App Resources
 ## The Issue
-We've noticed that there is a latency between receiving a confirmation of an update to an item's access and having that new access level reflected on the item (at least when the item is loaded through the search endpoint--haven't yet tested others).
+We've noticed that user app resources can sometimes get into a state where they are no longer reported as existing, but we are prevented from creating a new resource with the same name (the API reports that it already exists). We are also prevented from removing the resource (the API reports that it does NOT exist).
 
-This demonstration code attempts to toggle six items between `public` and `org` access levels. It illustrates that when the items are requested immediately after we receive a "success" response from all the sharing calls, the item access levels are often out-of-date.
-
-However, when given a one-second delay before requesting the updated items the access levels are correct.
+There is nothing we can do to recover from this state.
 
 ## Running the Demonstration Code
 1. Run `npm i` to install dependencies.
 1. Rename the `.env-example` file to `.env`.
-1. Supply the item owner's credentials in the new `.env`. We will provide them through a secure channel.
+1. Supply the credentials in `.env`. We will provide them through a secure channel.
 1. Run the following command:
 ```
 node index.js
@@ -18,37 +16,16 @@ node index.js
 ## Example Output
 
 ```
-TEST 1
--------------------------------------------
-
-Here's how the items are now.
-
-12d7b2a7bdc84e3885d20ffa0784b5dd: public
-be31b2c465834508b8937737e12cc570: public
-51bfd3e2b5bc4c6c87a08fc19e152c81: public
-0144b5e434484221a4cef2a88078c7e7: public
-beca445f5d114fae8b2ff154ea33739a: public
-7eec561b24c843e5b49aca585000e0be: public
-
-Setting all items to ORG
-
-DONE!
-
-Requesting items again immediately.
-
-12d7b2a7bdc84e3885d20ffa0784b5dd: public
-be31b2c465834508b8937737e12cc570: public
-51bfd3e2b5bc4c6c87a08fc19e152c81: public
-0144b5e434484221a4cef2a88078c7e7: public
-beca445f5d114fae8b2ff154ea33739a: public
-7eec561b24c843e5b49aca585000e0be: public
-
-Requesting items again after a pause.
-
-12d7b2a7bdc84e3885d20ffa0784b5dd: org
-be31b2c465834508b8937737e12cc570: org
-51bfd3e2b5bc4c6c87a08fc19e152c81: org
-0144b5e434484221a4cef2a88078c7e7: org
-beca445f5d114fae8b2ff154ea33739a: org
-7eec561b24c843e5b49aca585000e0be: org
+CURRENT USER RESOURCES
+[
+  '-KOikkCXTvVZUFAwQFWHQ.json',
+  '518xhxqoYpmZKB7COUTGy.json',
+  '6HPQQ0Qj-jJhlat8ZtQWd.json',
+  'user_settings.json'
+]
+NO ENTRY FOR hub-user-settings.json
+TRY ADDING hub-user-settings.json
+ERROR: COM_1169: A resource with the specified key already exists for the user.
+TRY REMOVING hub-user-settings.json
+ERROR: COM_1177: User resource does not exist or is inaccessible.
 ```
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('cross-fetch/polyfill');
 require('isomorphic-form-data');
 
 const { UserSession } = require('@esri/arcgis-rest-auth');
-const { searchItems, setItemAccess } = require('@esri/arcgis-rest-portal');
+const { request } = require('@esri/arcgis-rest-request');
 
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
@@ -18,68 +18,64 @@ const session = new UserSession({
   portal
 });
 
-const itemIds = [
-  '7eec561b24c843e5b49aca585000e0be',
-  '51bfd3e2b5bc4c6c87a08fc19e152c81',
-  '0144b5e434484221a4cef2a88078c7e7',
-  'be31b2c465834508b8937737e12cc570',
-  'beca445f5d114fae8b2ff154ea33739a',
-  '12d7b2a7bdc84e3885d20ffa0784b5dd'
-];
+const basicRequestOpts = {
+  username,
+  portal,
+  authentication: session
+};
 
-function formatAccessLevels(items) {
-  const accesses = items.map(item => `${item.id}: ${item.access}\n`);
-  return accesses.join('');
+function addUserResource (requestOptions) {
+  return request(`${requestOptions.portal}/community/users/${requestOptions.username}/addResource`, requestOptions);
 }
 
-function toggleAccess(access) {
-  return access === 'public' ? 'org' : 'public';
+function getUserResource (requestOptions) {
+  return request(`${requestOptions.portal}/community/users/${requestOptions.username}/resources/${requestOptions.name}`, requestOptions);
 }
 
-async function getItems(itemIds) {
-  const { results } = await searchItems({
-    q: 'id:' + itemIds.join(' OR id:'),
-    authentication: session
-  });
-  return results;
+function listUserResources (requestOptions) {
+  return request(`${requestOptions.portal}/community/users/${requestOptions.username}/resources`, requestOptions);
 }
 
-function pause (seconds) {
-  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+function removeUserResource (requestOptions) {
+  return request(`${requestOptions.portal}/community/users/${requestOptions.username}/removeResource`, requestOptions);
 }
 
-async function runTest() {
-  console.log(`Here's how the items are now.\n`);
-  const beforeItems = await getItems(itemIds);
-  console.log(formatAccessLevels(beforeItems));
-
-  const newAccess = toggleAccess(beforeItems[0].access);
-  console.log(`Setting all items to ${newAccess.toUpperCase()}\n`);
-
-  await Promise.all(beforeItems.map(item => setItemAccess({
-    id: item.id,
-    owner: item.owner,
-    access: newAccess,
-    authentication: session
-  })));
-  console.log(`DONE!\n`);
-
-  console.log(`Requesting items again immediately.\n`);
-  const afterItems = await getItems(itemIds);
-  console.log(formatAccessLevels(afterItems));
-
-  // wait for things to settle
-  await pause(1);
-
-  console.log(`Requesting items again after a pause.\n`);
-  const wayafterItems = await getItems(itemIds);
-  console.log(formatAccessLevels(wayafterItems));
-}
+const resourceName = 'hub-user-settings.json';
+const resourceContents = { foo: 'bar' };
 
 (async function main () {
-  for (let i = 0; i < 3; i++) {
-    console.log(`TEST ${i+1}`)
-    console.log(`-------------------------------------------\n`)
-    await runTest();
+
+  const { userResources } = await listUserResources(basicRequestOpts);
+  console.log('CURRENT USER RESOURCES')
+  console.log(userResources.map(resource => resource.key));
+
+  console.log(`NO ENTRY FOR ${resourceName}`)
+
+  console.log(`TRY ADDING ${resourceName}`)
+  try {
+    await addUserResource({
+      ...basicRequestOpts,
+      params: {
+        // file: objectToJsonBlob(settingsObj), may have to do this later if we need to bypass sanitization
+        text: JSON.stringify(resourceContents),
+        access: 'UserPrivateAllApps',
+        key: resourceName
+      }
+    })
+  } catch (err) {
+    console.error('ERROR: ' + err.message);
+  }
+
+  console.log(`TRY REMOVING ${resourceName}`)
+  try {
+    // remove resource
+    await removeUserResource({
+      ...basicRequestOpts,
+      params: {
+        key: resourceName
+      }
+    })
+  } catch (err) {
+    console.error('ERROR: ' + err.message);
   }
 })();
